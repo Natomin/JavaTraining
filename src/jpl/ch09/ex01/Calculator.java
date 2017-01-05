a35d85af9afbb8c4bff78717348b2c828f0f95e9
藤村奈都美 <fujimuranatsumi@fujimurakyouotoko-no-MacBook-Air-2.local>
2017-01-05 23:27:50 +0900

    メソッドの引数をenum型に変更

--- src/jpl/ch09/ex01/Calculator.java
+++ src/jpl/ch09/ex01/Calculator.java
@@ -1,25 +1,81 @@
 package jpl.ch09.ex01;
 
 public class Calculator {
+	public enum Infinity{ NEGATIVE_INFINITY, POSITIVE_INFINITY }
 	
-	public void add(double x, double y) {
+	public double add(Infinity a, Infinity b) {
+		double x = 0;
+		double y = 0;
 		double result;
+		if(a == Infinity.NEGATIVE_INFINITY){
+			x = Double.NEGATIVE_INFINITY;
+		}else if(a == Infinity.POSITIVE_INFINITY){
+			x = Double.POSITIVE_INFINITY;
+		}
+		if(b == Infinity.NEGATIVE_INFINITY){
+			y = Double.NEGATIVE_INFINITY;
+		}else if(b == Infinity.POSITIVE_INFINITY){
+			y = Double.POSITIVE_INFINITY;
+		}
 		result = x + y;
 		System.out.println(x + " + " + y +" = " + result);
+		return result;
 	}
-	public void subtract(double x, double y) {
+	
+	public double subtract(Infinity a, Infinity b) {
+		double x = 0;
+		double y = 0;
 		double result;
+		if(a == Infinity.NEGATIVE_INFINITY){
+			x = Double.NEGATIVE_INFINITY;
+		}else if(a == Infinity.POSITIVE_INFINITY){
+			x = Double.POSITIVE_INFINITY;
+		}
+		if(b == Infinity.NEGATIVE_INFINITY){
+			y = Double.NEGATIVE_INFINITY;
+		}else if(b == Infinity.POSITIVE_INFINITY){
+			y = Double.POSITIVE_INFINITY;
+		}
 		result = x - y;
 		System.out.println(x + " - " + y +" = " + result);
+		return result;
 	}
-	public void multiply(double x, double y) {
+	
+	public double multiply(Infinity a, Infinity b) {
+		double x = 0;
+		double y = 0;
 		double result;
+		if(a == Infinity.NEGATIVE_INFINITY){
+			x = Double.NEGATIVE_INFINITY;
+		}else if(a == Infinity.POSITIVE_INFINITY){
+			x = Double.POSITIVE_INFINITY;
+		}
+		if(b == Infinity.NEGATIVE_INFINITY){
+			y = Double.NEGATIVE_INFINITY;
+		}else if(b == Infinity.POSITIVE_INFINITY){
+			y = Double.POSITIVE_INFINITY;
+		}
 		result = x * y;
 		System.out.println(x + " × " + y +" = " + result);
+		return result;
 	}
-	public void divide(double x, double y) {
+
+	public double divide(Infinity a, Infinity b) {
+		double x = 0;
+		double y = 0;
 		double result;
+		if(a == Infinity.NEGATIVE_INFINITY){
+			x = Double.NEGATIVE_INFINITY;
+		}else if(a == Infinity.POSITIVE_INFINITY){
+			x = Double.POSITIVE_INFINITY;
+		}
+		if(b == Infinity.NEGATIVE_INFINITY){
+			y = Double.NEGATIVE_INFINITY;
+		}else if(b == Infinity.POSITIVE_INFINITY){
+			y = Double.POSITIVE_INFINITY;
+		}
 		result = x / y;
 		System.out.println(x + " ÷ " + y +" = " + result);
+		return result;
 	}
 }
--- src/jpl/ch09/ex01/Main.java
+++ src/jpl/ch09/ex01/Main.java
@@ -1,21 +1,23 @@
 package jpl.ch09.ex01;
 
+import jpl.ch09.ex01.Calculator.Infinity;
+
 public class Main {
 
 	public static void main(String[] args) {
 		Calculator calculator = new Calculator();
-		calculator.add(Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
-		calculator.add(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
-		calculator.add(Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY);
-		calculator.subtract(Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
-		calculator.subtract(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
-		calculator.subtract(Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY);
-		calculator.multiply(Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
-		calculator.multiply(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
-		calculator.multiply(Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY);
-		calculator.divide(Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
-		calculator.divide(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
-		calculator.divide(Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY);
+		calculator.add(Infinity.NEGATIVE_INFINITY, Infinity.POSITIVE_INFINITY);
+		calculator.add(Infinity.POSITIVE_INFINITY, Infinity.POSITIVE_INFINITY);
+		calculator.add(Infinity.NEGATIVE_INFINITY, Infinity.NEGATIVE_INFINITY);
+		calculator.subtract(Infinity.NEGATIVE_INFINITY, Infinity.POSITIVE_INFINITY);
+		calculator.subtract(Infinity.POSITIVE_INFINITY, Infinity.POSITIVE_INFINITY);
+		calculator.subtract(Infinity.NEGATIVE_INFINITY, Infinity.NEGATIVE_INFINITY);
+		calculator.multiply(Infinity.NEGATIVE_INFINITY, Infinity.POSITIVE_INFINITY);
+		calculator.multiply(Infinity.POSITIVE_INFINITY, Infinity.POSITIVE_INFINITY);
+		calculator.multiply(Infinity.NEGATIVE_INFINITY, Infinity.NEGATIVE_INFINITY);
+		calculator.divide(Infinity.NEGATIVE_INFINITY, Infinity.POSITIVE_INFINITY);
+		calculator.divide(Infinity.POSITIVE_INFINITY, Infinity.POSITIVE_INFINITY);
+		calculator.add(Infinity.NEGATIVE_INFINITY, Infinity.NEGATIVE_INFINITY);
 	}
 
 }
