9e93dc6f74157ef4d5c2cb4173a4465bbdf897c5
藤村奈都美 <fujimuranatsumi@fujimurakyouotoko-no-MacBook-Air-2.local>
2017-01-25 06:58:49 +0900

    軽微な修正

--- src/jpl/ch03/ex07/ColorAttr.java
+++ src/jpl/ch03/ex07/ColorAttr.java
@@ -1,9 +1,14 @@
 package jpl.ch03.ex07;
 //equalsとhashCodeをオーバーライドする
+
+//スーパークラスのtoStringメドッドをオーバーライドしていないのでこのままだとequalsメソッドは色が違ってもturueになってしまう。
+//nameとvakueとmyColorの３つのフィールドを比較する必要がある
+//まず型チェックする（自分のクラスと同じインスタンスじゃなかったらfalse）型が一緒だったら３つのフィールドを比較
+//
 public class ColorAttr extends Attr{
 	private ScreenColor myColor;
 	
-	public boolean equals(Object obj){//オーバーロード
+	public boolean equals(Object obj){//オーバーライド
 		if(this.toString().equals(obj.toString())){
 			return true;
 		}else{
@@ -11,7 +16,7 @@ public class ColorAttr extends Attr{
 		}
 	}
 	
-	public int hashCode(){//オーバーロード
+	public int hashCode(){//オーバーライド
 		int resreturn = myColor.hashCode() * 2;
 		return resreturn;
 	}
--- src/jpl/ch03/ex11/SimpleSortDouble.java
+++ src/jpl/ch03/ex11/SimpleSortDouble.java
@@ -1,7 +1,11 @@
 package jpl.ch03.ex11;
-
+//subclassのdoSortで不正をするという意味の問題
+//doSort
+//データもらう⇒メトリクスをクリア⇒ソート
+//①もう一度スーパークラスのsortを呼んでソートすると初期化
+//②Probe()を使えばすべてのデータが手に入る⇒データを手に入れてソートしてからsortを呼ぶ
+//③メトリクスをオーバーフローさせる（現実的には困難）
 public class SimpleSortDouble extends SortDouble{
-
 	@Override
 	protected void doSort() {
 		for(int i = 0; i < getDataLength(); i++){
