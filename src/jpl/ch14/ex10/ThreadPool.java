1cce4e66be2faeab452da236572bddcf3a5eeb76
藤村奈都美 <fujimuranatsumi@fujimurakyouotoko-no-MacBook-Air-2.local>
2017-03-03 01:44:41 +0900

    修正

--- src/jpl/ch14/ex10/ThreadPool.java
+++ src/jpl/ch14/ex10/ThreadPool.java
@@ -4,68 +4,70 @@ public class ThreadPool {
 	private boolean isStart;
 	private Queue queue;
 	private LoopThread[] thread;
-	
-	public ThreadPool(int queueSize, int numberOfThreads){
-		if(queueSize < 1 || numberOfThreads < 1){
+
+	public ThreadPool(int queueSize, int numberOfThreads) {
+		if (queueSize < 1 || numberOfThreads < 1) {
 			throw new IllegalArgumentException();
 		}
 		queue = new Queue(queueSize);
 		thread = new LoopThread[numberOfThreads];
-		for(int i = 0; i < thread.length; i++){
+		for (int i = 0; i < thread.length; i++) {
 			thread[i] = new LoopThread();
 		}
-//		throw new AssertionError("Not Implemented Yet");
+		// throw new AssertionError("Not Implemented Yet");
 	}
-	
-	public void start(){
-		if(isStart){
+
+	public void start() {
+		if (isStart) {
 			throw new IllegalStateException();
 		}
-		for(int i = 0; i < thread.length; i++){
+		for (int i = 0; i < thread.length; i++) {
 			thread[i].start();
 		}
 		isStart = true;
-//		throw new AssertionError("Not Implemented Yet");
+		// throw new AssertionError("Not Implemented Yet");
 	}
-	
-	public void stop(){
-		if(!isStart){
+
+	public void stop() {
+		if (!isStart) {
 			throw new IllegalStateException();
 		}
-		
-		for(int i = 0; i < thread.length; i++){
+
+		for (int i = 0; i < thread.length; i++) {
 			thread[i].finnish();
 		}
-//		throw new AssertionError("Not Implemented Yet");
+		// while(Thread.activeCount() > 2){
+		// //待ちループ
+		// }
+		// throw new AssertionError("Not Implemented Yet");
 	}
-	
-	public synchronized void dispatch(Runnable runnable){
-		queue.push(runnable);
+
+	public synchronized void dispatch(Runnable runnable) {
+		if (runnable != null) {
+			queue.push(runnable);
+		}
 		throw new AssertionError("Not Implemented Yet");
+		// throw new NullPointerException();
 	}
-	
-	
-	private class LoopThread extends Thread{
+
+	private class LoopThread extends Thread {
 		private boolean isActive = true;
-		public void run(){
-			
-			while(isActive){
-				if(queue.pop() != null){
+
+		public void run() {
+
+			while (isActive) {
+				if (queue.pop() != null) {
 					queue.pop().run();
 				}
 			}
-			while(queue.pop() != null){
+			while (queue.pop() != null) {
 				queue.pop().run();
 			}
 		}
-		
-		public void finnish(){
+
+		public void finnish() {
 			isActive = false;
 		}
 	}
-		
-		
-		
-		
 
 }
