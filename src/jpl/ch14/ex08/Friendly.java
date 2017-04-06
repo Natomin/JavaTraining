a486691dec12e3efc0be2b2f16de973dc8d23a32
藤村奈都美 <fujimuranatsumi@fujimurakyouotoko-no-MacBook-Air-2.local>
2017-04-07 01:42:59 +0900

    デッドロック回避のための修正を追加

--- src/jpl/ch14/ex08/Friendly.java
+++ src/jpl/ch14/ex08/Friendly.java
@@ -1,25 +1,32 @@
 package jpl.ch14.ex08;
 
+//yieldメソッド呼び出しを追加したら頻度を変更できるか？→できない。
+//一方のスレッドが他方のスレッドにCPUサイクルを渡したとしても相互にロックする状態は変わらないため
+//同期を解除することなくデッドロックの可能性を取り除く→共通のlockオブジェクトを用いることでデッドロックを回避する
 public class Friendly {
+	private static final Object lock = new Object();
 	private Friendly partner;
 	private String name;
-	
-	public Friendly(String name){
+
+	public Friendly(String name) {
 		this.name = name;
 	}
-	
-	public synchronized void hug(){
-		System.out.println(Thread.currentThread().getName() + 
-		"in" + name + ".hug() tryint to invoke " + partner.name + ".hugBack()");
-		partner.hugBack();
+
+	public void hug() {
+		synchronized (lock) {
+			System.out.println(Thread.currentThread().getName() + "in" + name + ".hug() tryint to invoke "
+					+ partner.name + ".hugBack()");
+			partner.hugBack();
+		}
 	}
-	
-	private synchronized void hugBack(){
-		System.out.println(Thread.currentThread().getName() + 
-				"in" + name + ".hugBack()");
+
+	private void hugBack() {
+		synchronized (lock) {
+			System.out.println(Thread.currentThread().getName() + "in" + name + ".hugBack()");
+		}
 	}
-	
-	public void becomeFriend(Friendly partner){
+
+	public void becomeFriend(Friendly partner) {
 		this.partner = partner;
 	}
 
