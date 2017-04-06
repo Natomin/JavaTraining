e0923900fe1ef84142ba1c2236299193bdc53168
藤村奈都美 <fujimuranatsumi@fujimurakyouotoko-no-MacBook-Air-2.local>
2017-04-07 02:26:52 +0900

    追加修正。test12/19

--- src/jpl/ch14/ex10/Queue.java
+++ src/jpl/ch14/ex10/Queue.java
@@ -5,27 +5,33 @@ import java.util.ArrayList;
 public class Queue {
 	private int queueSize;
 	private ArrayList<Runnable> queue;
-	
-	public Queue(int queueSize){
+
+	public Queue(int queueSize) {
 		this.queueSize = queueSize;
 		queue = new ArrayList<Runnable>(queueSize);
 	}
-	
-	public synchronized void push(Runnable runnable){
-		while(queueSize <= queue.size()){
-			//待ちループ
+
+	public synchronized void push(Runnable runnable) {
+		while (queueSize <= queue.size()) {
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				// TODO 自動生成された catch ブロック
+				e.printStackTrace();
+			}
+			// queueにtaskが入りきらないので待つ
 		}
 		queue.add(runnable);
 	}
-	
-	public synchronized Runnable pop(){
-		if(queue.size() < 1){
+
+	public synchronized Runnable pop() {
+		if (queue.size() < 1) {
 			return null;
 		}
 		return queue.remove(0);
 	}
-	
-	public int getQueueSize(){
+
+	public int getQueueSize() {
 		return queue.size();
 	}
 
--- src/jpl/ch14/ex10/ThreadPool.java
+++ src/jpl/ch14/ex10/ThreadPool.java
@@ -1,10 +1,36 @@
+/*
+ * Copyright (C) 2012, 2013, 2016 RICOH Co., Ltd. All rights reserved.
+ */
 package jpl.ch14.ex10;
 
+/**
+ * Simple Thread Pool class.
+ *
+ * This class can be used to dispatch an Runnable object to be exectued by a
+ * thread.
+ *
+ * [Instruction] Implement one constructor and three methods. Don't forget to
+ * write a Test program to test this class. Pay attention to @throws tags in the
+ * javadoc. If needed, you can put "synchronized" keyword to methods. All
+ * classes for implementation must be private inside this class. Don't use
+ * java.util.concurrent package.
+ */
 public class ThreadPool {
 	private boolean isStart;
 	private Queue queue;
 	private LoopThread[] thread;
 
+	/**
+	 * Constructs ThreadPool.
+	 *
+	 * @param queueSize
+	 *            the max size of queue
+	 * @param numberOfThreads
+	 *            the number of threads in this pool.
+	 *
+	 * @throws IllegalArgumentException
+	 *             if either queueSize or numberOfThreads is less than 1
+	 */
 	public ThreadPool(int queueSize, int numberOfThreads) {
 		if (queueSize < 1 || numberOfThreads < 1) {
 			throw new IllegalArgumentException();
@@ -14,40 +40,77 @@ public class ThreadPool {
 		for (int i = 0; i < thread.length; i++) {
 			thread[i] = new LoopThread();
 		}
-		// throw new AssertionError("Not Implemented Yet");
 	}
 
+	/**
+	 * Starts threads.
+	 *
+	 * @throws IllegalStateException
+	 *             if threads has been already started.
+	 */
 	public void start() {
-		if (isStart) {
-			throw new IllegalStateException();
+
+		synchronized (this) {
+			if (isStart) {
+				throw new IllegalStateException();
+			}
+			isStart = true;
 		}
-		for (int i = 0; i < thread.length; i++) {
-			thread[i].start();
+		for (LoopThread l : thread) {
+			l.start();
 		}
-		isStart = true;
-		// throw new AssertionError("Not Implemented Yet");
+		System.out.println("start:" + Thread.activeCount());
 	}
 
-	public void stop() {
+	/**
+	 * Stop all threads and wait for their terminations.
+	 *
+	 * @throws IllegalStateException
+	 *             if threads has not been started.
+	 */
+	public synchronized void stop() {
+		System.out.println("stop:" + Thread.activeCount());
 		if (!isStart) {
 			throw new IllegalStateException();
 		}
-
-		for (int i = 0; i < thread.length; i++) {
-			thread[i].finnish();
+		for (LoopThread l : thread) {
+			l.finnish();
 		}
-		// while(Thread.activeCount() > 2){
-		// //待ちループ
-		// }
-		// throw new AssertionError("Not Implemented Yet");
+		int activeCount;
+		do{
+			activeCount = 0;
+			for(LoopThread l : thread){
+				if(l.isAlive()){
+					activeCount++;
+				}
+			}
+		} while (activeCount > 0);
+		
+		System.out.println("stop2:" + Thread.activeCount());
 	}
 
+	/**
+	 * Executes the specified Runnable object, using a thread in the pool. run()
+	 * method will be invoked in the thread. If the queue is full, then this
+	 * method invocation will be blocked until the queue is not full.
+	 * 
+	 * @param runnable
+	 *            Runnable object whose run() method will be invoked.
+	 *
+	 * @throws NullPointerException
+	 *             if runnable is null.
+	 * @throws IllegalStateException
+	 *             if this pool has not been started yet.
+	 */
 	public synchronized void dispatch(Runnable runnable) {
+		if (!isStart) {
+			throw new IllegalStateException();
+		}
 		if (runnable != null) {
 			queue.push(runnable);
+		} else {
+			throw new NullPointerException();
 		}
-		throw new AssertionError("Not Implemented Yet");
-		// throw new NullPointerException();
 	}
 
 	private class LoopThread extends Thread {
@@ -55,13 +118,11 @@ public class ThreadPool {
 
 		public void run() {
 
-			while (isActive) {
-				if (queue.pop() != null) {
-					queue.pop().run();
+			Runnable task;
+			while (((task = queue.pop()) != null) || isActive) {
+				if (task != null) {
+					task.run();
 				}
-			}
-			while (queue.pop() != null) {
-				queue.pop().run();
 			}
 		}
 
