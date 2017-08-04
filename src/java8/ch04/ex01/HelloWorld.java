3122991c1a39985eba6b379c76d8a700a8a9841d
藤村奈都美 <fujimuranatsumi@fujimurakyouotoko-no-MacBook-Air-2.local>
2017-08-04 10:05:07 +0900

    練習問題の追加

---
+++ src/java8/ch04/ex01/HelloWorld.java
@@ -0,0 +1,32 @@
+package java8.ch04.ex01;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class HelloWorld extends Application{
+	
+	@Override
+	public void start(Stage stage) {
+		Label message = new Label("Hello, JavaFX!");
+	    message.setFont(new Font(100));
+	    
+	    TextField messageField = new TextField(message.getText());
+	    messageField.textProperty().addListener(property -> message.setText(messageField.getText()));
+	
+	    VBox vRayout = new VBox();
+	    vRayout.getChildren().addAll(message, messageField);
+	    stage.setScene(new Scene(vRayout));
+	    stage.setTitle("title");
+	    stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
